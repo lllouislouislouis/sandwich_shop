6c2b7c8346f8582988833bf386ce2a700ee31e7c
lllouislouislouis <louisjeds@outlook.com>
2025-10-24 08:13:43 +0100

    Refactor main.dart to update the home page layout and display multiple order items; update widget test to use the correct App widget

--- lib/main.dart
+++ lib/main.dart
@@ -6,15 +6,31 @@ void main() {
 
 class App extends StatelessWidget {
   const App({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Sandwich Shop App',
       home: Scaffold(
         appBar: AppBar(title: const Text('Sandwich Counter')),
-        body: const Center(
-          child: OrderItemDisplay(5, 'Footlong'),
+        body: Center(
+          child: Container(
+            color: Colors.red,
+            width: 350,
+            height: 350,
+            alignment: Alignment.center,
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment
+                  .spaceEvenly, // try center, spaceAround, spaceBetween, etc.
+              crossAxisAlignment:
+                  CrossAxisAlignment.center, // try start, end, stretch
+              children: const [
+                OrderItemDisplay(5, 'Footlong'),
+                OrderItemDisplay(3, 'BLT'),
+                OrderItemDisplay(1, 'Club'),
+              ],
+            ),
+          ),
         ),
       ),
     );
@@ -29,6 +45,13 @@ class OrderItemDisplay extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text('$quantity $itemType sandwich(es): ${'🥪' * quantity}');
+    return Text(
+      '$quantity $itemType sandwich(es): ${'🥪' * quantity}',
+      style: TextStyle(
+        fontSize: 20.0, //+ quantity.toDouble() * 2.0,
+        color: Colors.green,
+        fontWeight: FontWeight.bold,
+      ),
+    );
   }
 }
