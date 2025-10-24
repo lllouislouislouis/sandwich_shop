3dee2d089948bda8177c5a43fdd04bff7052ba1c
lllouislouislouis <louisjeds@outlook.com>
2025-10-24 09:18:59 +0100

    Use OrderScreen in App

--- lib/main.dart
+++ lib/main.dart
@@ -6,35 +6,11 @@ void main() {
 
 class App extends StatelessWidget {
   const App({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Sandwich Shop App',
-      home: Scaffold(
-        appBar: AppBar(title: const Text('Sandwich Counter')),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const OrderItemDisplay(5, 'Footlong'),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  ElevatedButton(
-                    onPressed: () => print('Add button pressed!'),
-                    child: const Text('Add'),
-                  ),
-                  ElevatedButton(
-                    onPressed: () => print('Remove button pressed!'),
-                    child: const Text('Remove'),
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
+    return const MaterialApp(
+        title: 'Sandwich Shop App', home: OrderScreen(maxQuantity: 5));
   }
 }
 
