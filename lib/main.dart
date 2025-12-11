fcb5dacb15cb59c342f5bdfff688db288f57a0a7
lllouislouislouis <louisjeds@outlook.com>
2025-12-11 09:49:38 +0000

    Remove unused imports and simplify Cart provider setup in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:sandwich_shop/models/cart.dart';
 import 'package:sandwich_shop/views/order_screen.dart';
-import 'package:sandwich_shop/views/checkout_screen.dart';
-import 'package:sandwich_shop/views/about_screen.dart';
-import 'package:sandwich_shop/views/auth_screen.dart';
 
 void main() {
   runApp(const App());
@@ -16,28 +13,13 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
-      create: (context) => Cart(),
-      child: MaterialApp(
+      create: (BuildContext context) {
+        return Cart();
+      },
+      child: const MaterialApp(
         title: 'Sandwich Shop App',
-        initialRoute: '/',
-        routes: {
-          '/': (context) => const OrderScreen(maxQuantity: 5),
-          '/about': (context) => const AboutScreen(),
-          '/auth': (context) => const AuthScreen(),
-        },
-        onGenerateRoute: (settings) {
-          // Handle the checkout route specially to pass the cart
-          if (settings.name == '/checkout') {
-            return MaterialPageRoute(
-              settings: settings,
-              builder: (context) {
-                final cart = Provider.of<Cart>(context, listen: false);
-                return CheckoutScreen(cart: cart);
-              },
-            );
-          }
-          return null;
-        },
+        debugShowCheckedModeBanner: false,
+        home: OrderScreen(maxQuantity: 5),
       ),
     );
   }
